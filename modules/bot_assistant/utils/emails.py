bac0a65afcdec9c44f9d66b9b861870d717ca57c
EugeneChurylov <e.churylov@gmail.com>
2023-10-31 16:16:51 +0100

    repl the regex

--- modules/bot_assistant/utils/emails.py
+++ modules/bot_assistant/utils/emails.py
@@ -7,7 +7,7 @@ def validate_email(email):
     :param email:
     """
     # john@email.com
-    pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    pattern = re.compile(r"\b[A-Za-z][A-Za-z0-9._]+@[A-Za-z]+\.[A-Za-z]{2,}\b")
     return bool(pattern.match(email))
 
 
